82e857ada8ee3a56731fd6807f1af317f90e3bb2
Super-Web112 <74626440+Super-Web112@users.noreply.github.com>
2021-03-26 13:07:15 -0700

    version 2

--- js/script3.js
+++ js/script3.js
@@ -1,38 +1,48 @@
 $(document).ready(function(){
     var obj = $('#dog_o')
-    var r_width = obj.width()
-    var r_height = obj.height()
-   
-    var temp = new Array()
-    var count = 1
-    temp[0] = 0
-
-    var p_left, p_top, c_left, c_top, centerPositionX, centerPositionY
+    var r_size = obj.css('background-size')
     $('#btn_up').click(function(){
         move_up(obj)
+        var full_height = $(".box-container").height()
         p_top = obj.css('margin-top')
-        $("#coord_y").html(p_top)
+        var p_t = Number(p_top.replace('px',''))
+        var r_dis = (p_t/full_height)*100
+        r_dis = r_dis.toFixed(2)
+        $("#coord_y").html(r_dis)
     })
 
     $('#btn_down').click(function(){
         move_down(obj)
+        var full_height = $(".box-container").height()
         p_top = obj.css('margin-top')
-        $("#coord_y").html(p_top)
+        var p_t = Number(p_top.replace('px',''))
+        var r_dis = (p_t/full_height)*100
+        r_dis = r_dis.toFixed(2)
+        $("#coord_y").html(r_dis)
     })
 
     $('#btn_left').click(function(){
         move_left(obj)
+        var full_width = $(".box-container").width()
         p_left = obj.css('margin-left')
-        $("#coord_x").html(p_left)
+        var p_l = Number(p_left.replace('px',''))
+        var r_dis = (p_l/full_width)*100
+        r_dis = r_dis.toFixed(2)
+        $("#coord_x").html(r_dis)
     })
 
     $('#btn_right').click(function(){
         move_right(obj)
+        var full_width = $(".box-container").width()
         p_left = obj.css('margin-left')
-        $("#coord_x").html(p_left)
+        var p_l = Number(p_left.replace('px',''))
+        var r_dis = (p_l/full_width)*100
+        r_dis = r_dis.toFixed(2)
+        $("#coord_x").html(r_dis)
     })
 
     $("div#slider").slider({
+        value: 50,
         animate: true,                  
         start: function (event)
         {
@@ -49,62 +59,46 @@ $(document).ready(function(){
         slide: function (event)
         {
             var value = $("div#slider").slider("value");
-            
-            temp[count] = value
-            p_left = obj.css('margin-left')
-            p_top = obj.css('margin-top')
-            c_left = Number(p_left.replace("px",""))
-            c_top = Number(p_top.replace("px",""))
-            
-            if(temp[count]>temp[count-1]){
-                centerPositionX = c_left-1
-                centerPositionY = c_top-1
-            }else{
-                centerPositionX = c_left+1
-                centerPositionY = c_top+1
-            }
-
-            zoom(obj, value*2, r_width, r_height)
-            obj.css('margin-left',centerPositionX)
-            obj.css('margin-top',centerPositionY)
-
-            $("#valueSlide").html("1."+(value<10?('0'+value):value)+"x")
-            count++
+            var point, setValue
+            point = value/2 - 25
+            setValue = Number(r_size.replace('%',''))
+            setValue += point
+            console.log(point, r_size, setValue)
+            zoom(obj, setValue)
         } 
     });
 })
 
 function move_up(e){
     e.animate({
-        'marginTop' : "-=30px" //moves up
+        'marginTop' : "-=10px" //moves up
     }, 'fast');
     p = e.offset();
 }
 
 function move_down(e){
     e.animate({
-        'marginTop' : "+=30px" //moves down
+        'marginTop' : "+=10px" //moves down
     }, 'fast');
     p = e.offset();
 }
 
 function move_left(e){
     e.animate({
-        'marginLeft' : "-=30px" //moves left
+        'marginLeft' : "-=10px" //moves left
     }, 'fast');
     p = e.offset();
 }
 
 function move_right(e){
     e.animate({
-        'marginLeft' : "+=30px" //moves right
+        'marginLeft' : "+=10px" //moves right
     }, 'fast');
     p = e.offset();
 }
 
-function zoom(e, zx, r_width, r_height){
-    var d_width = Number(r_width + zx) + 'px'
-    var d_height = Number(r_height + zx) + 'px'
-    e.width(d_width)
-    e.height(d_height)
+function zoom(e, zx){
+    e.css('background-size', zx+'%')
+    var rate = (zx - 140)/5
+    $("#valueSlide").html(rate+'%')
 }
